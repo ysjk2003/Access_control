ca8d46efdcb5d1bf5111e4d4ce4c060aa6de22cb
ysjk2003 <ysjk2003@gmail.com>
2019-01-30 08:53:20 +0900

    modified URL collector

--- URL_list/URL_collector.py
+++ URL_list/URL_collector.py
@@ -5,9 +5,13 @@ from bs4 import BeautifulSoup
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", "--url", help="Target URL")
 
+args = parser.parse_args()
 target = open("target.txt", 'w')
 source_code = requests.get(args.url)
 plain_txt = source_code.text
+soup = BeautifulSoup(plain_txt, 'lxml')
 for link in soup.select('a'):
-    href = args.url + link.get('href')
-    target.write(href)
+    href = link.get('href')
+    target.write(href+"\n")
+
+target.close()
